9e09453d576deb83f6348a81b50c313dcff678b7
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-19 14:48:34 -0700

    Added login routes and forms

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,15 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/user/create' do
+  puts params.inspect
+  # new_user = User.create(params)
+  # Look in app/views/index.erb
+  # erb :decks
+end
+
+post '/user/login' do
+  # Look in app/views/index.erb
+  erb :decks
+end
