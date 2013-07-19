65247ba20a9e06759d91a96b6cdca89009c19fae
Apprentice <apprentice@dbc09.local>
2013-07-19 12:04:38 -0700

    First Commit create user and round tables

---
+++ db/migrate/20130719114538_create_tables.rb
@@ -0,0 +1,15 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string  :username
+      t.timestamps 
+    end
+    create_table :rounds do |t|
+      t.belongs_to :user
+      t.belongs_to :deck
+      t.timestamps
+    end
+  end
+
+end
+
